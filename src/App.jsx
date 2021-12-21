81bb02277a8b6bacd04940e48fc07d1988a8bce5
Dalvin <dalvin.josias@gmail.com>
2021-12-21 01:08:16 -0800

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useWeb3 } from "@3rdweb/hooks";
+
+
 const App = () => {
+
+  const { connectWallet, address, error, provider} = useWeb3();
+  console.log("Address:", address);
+
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to The Haitan DAO</h1>
+        <p>Funds in this treasury will be used to create access to tech in Haiti</p>
+      </div>
+    );
+  }
+
+
   return (
     <div className="landing">
-      <h1>Welcome to The Haitan DAO</h1>
-      <p>Funds in this treasury will be used to create access to tech in Haiti</p>
+      <h1>🇭🇹 wallet connected, now what!</h1>
     </div>
-  );
+  )
 };
 
 export default App;
