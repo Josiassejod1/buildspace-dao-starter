0c6bc8a2bfe88b532026a1ca59af2e8aac4e7f8d
Dalvin Josias Sejour <dalvinjosiassejour@Dalvins-Mac-mini.local>
2022-01-06 02:31:31 -0800

    fix

--- src/RoadMap.js
+++ src/RoadMap.js
@@ -4,16 +4,16 @@ import { CardBody, Card, CardTitle, CardText } from "reactstrap";
 
 const RoadMap = () => {
     return (
-        <div className="container" style={{ padding: 50 }}>
+        <div className="container" style={{ padding: 50, textAlign: "center" }} id="map">
             <Card>
                 <CardBody>
                     <CardTitle tag="h1">Our Road Map 🧭</CardTitle>
                     <div className="container" style={{ textAlign: "center", padding: 10 }}>
-                        <img src={haiti} width="125" />
+                        <img src={haiti} width="500" />
                     </div>
                     <CardText>
                         <p>Phase 1:</p>
-                        <p> 🇭🇹  Mint NFT And Make Donation To Non Profit</p>
+                        <p> 🇭🇹  Mint NFT And Make Donation To Non Profits</p>
                         <p>Phase 2:</p>
                         <p> 🇭🇹  Coming Soon</p>
                     </CardText>
